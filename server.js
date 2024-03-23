bd6a15fe718599b8305262f0c3e4e864d6386790
akbaraditamasp <akbaraditamasp@gmail.com>
2024-03-24 06:43:56 +0700

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({}));
+
+app.post("/locs", (req, res) => {
+  const locs = JSON.parse(
+    fs.readFileSync(path.resolve("location.json"), "utf-8")
+  );
+  locs.push({
+    lat: req.body.lat,
+    long: req.body.long,
+  });
+
+  fs.writeFileSync(path.resolve("location.json"), JSON.stringify(locs));
+
+  res.sendStatus(200);
+});
+
+app.get("/mobil-murah-jakarta", (req, res) => {
+  res.sendFile(path.resolve("index.html"));
+});
+
+app.listen(3100, () => {
+  console.log("launched");
+});
